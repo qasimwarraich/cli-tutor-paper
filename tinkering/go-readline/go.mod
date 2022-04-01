f3216f18915b08b5b4807d873a8ed302a9c2df93
qasimwarraich <qasim.warraich@gmail.com>
2022-04-01 09:03:27 +0200

    A shell of a 'shell', using readline in Go

---
+++ tinkering/go-readline/go.mod
@@ -0,0 +1,14 @@
+module go-readline
+
+go 1.17
+
+require github.com/chzyer/readline v0.0.0-20180603132655-2972be24d48e
+
+require (
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/mattn/go-runewidth v0.0.13 // indirect
+	github.com/muesli/termenv v0.11.0 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	golang.org/x/sys v0.0.0-20220330033206-e17cdc41300f // indirect
+)
---
+++ tinkering/go-readline/src/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/chzyer/readline"
+	"github.com/muesli/termenv"
+)
+
+func printer(s string, string_type string) {
+	p := termenv.ColorProfile()
+	styled := termenv.String(s)
+	switch string_type {
+	case "welcome":
+		fmt.Println(styled.Foreground(p.Color("#FF2FF2")))
+
+	case "error":
+		fmt.Println(styled.Foreground(p.Color("#FF0000")))
+
+	case "note":
+		fmt.Println(styled.Foreground(p.Color("#FFFF00")))
+
+	default:
+		fmt.Println(styled.Foreground(p.Color("#00FF00")))
+	}
+}
+
+func main() {
+	rl, err := readline.New("Chistole > ")
+	if err != nil {
+		panic(err)
+	}
+	defer rl.Close()
+
+	termenv.ClearScreen()
+
+	printer("Welcome to Chistole", "welcome")
+
+	time.Sleep(1 * time.Second)
+
+	printer("This lesson will be about", "")
+    printer(`Lorem ipsum dolor sit amet, officia excepteur ex fugiat
+    reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse
+    exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit
+    nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor
+    minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure
+    elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor
+    Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing
+    id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua
+    reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate
+    laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa
+    duis.`, "")
+
+	time.Sleep(4 * time.Second)
+    printer("\n\nWhen you are ready press any key to begin", "note")
+    fmt.Scanln() // Any key
+    
+	termenv.ClearScreen()
+
+	printer("Welcome to the shell", "")
+
+    //Readline loop
+	for {
+		line, err := rl.Readline()
+		if err != nil { // io.EOF
+			break
+		}
+		println(line)
+	}
+}
